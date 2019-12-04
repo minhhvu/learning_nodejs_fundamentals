45dac06592ccd80594cce0afc3db6f4e9a907837
minhh <minhhaivuhd@gmail.com>
2019-12-03 20:31:59 -0500

    add views for login and register pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Homepage' });
 });
 
+/* Login page */
+router.get('/login', (req, res) => {
+  res.render('login', {title: 'Login'})
+})
+
+/* Register page */
+router.get('/register', (req, res) => {
+  res.render('register', {title: 'Register'})
+})
+
 module.exports = router;
